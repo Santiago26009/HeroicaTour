1f2fa1ad4ef9df00615beb51836b9680ce286d6b
Santiago Zúñiga <39531781+Santiago26009@users.noreply.github.com>
2021-05-02 23:19:09 -0500

    Revert "commit heroku"
    This reverts commit 9f88e98cd4e0654338066c346c3639037c82d09a.

--- HeroicaTour_API/HeroicaTour_DB/views.py
+++ HeroicaTour_API/HeroicaTour_DB/views.py
@@ -17,7 +17,7 @@ from .models import Taxi as modeltaxi
 # Create your views here.
 class ClTrabajadorView(ListAPIView, CreateAPIView):
     serializer_class = serializer.CS_Trabajador
-    queryset = modeltrabajador.objects.filter(Rol='Guía')
+    queryset = modeltrabajador.objects.all()
     
 class ClClienteView(ListAPIView, CreateAPIView):
     serializer_class = serializer.CS_Cliente
