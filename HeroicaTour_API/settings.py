12b1ba3c01ade02ce1c5aa02315f2c68036823b9
Santiago Zúñiga <39531781+Santiago26009@users.noreply.github.com>
2021-05-07 11:21:09 -0500

    Allowed hosts

--- HeroicaTour_API/settings.py
+++ HeroicaTour_API/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-^tar(h0%90wvafiquvi=xxxdm7&e!r0_du5glqk8r4_qrtrc1l')
+SECRET_KEY = 'django-insecure-^tar(h0%90wvafiquvi=xxxdm7&e!r0_du5glqk8r4_qrtrc1l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['heroicatour.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -153,6 +153,3 @@ REST_FRAMEWORK = {
         'knox.auth.TokenAuthentication',
     ]
 }
-
-if config('DJANGO_PRODUCTION_ENV', default=False, cast=bool):
-    from .settings_pr import *
