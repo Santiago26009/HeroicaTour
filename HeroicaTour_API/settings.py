4d00fb0f7976e37f0e835f4cda800b58ddd72bd0
Santiago Zúñiga <39531781+Santiago26009@users.noreply.github.com>
2021-05-07 11:46:35 -0500

    Deploy

--- HeroicaTour_API/settings.py
+++ HeroicaTour_API/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^tar(h0%90wvafiquvi=xxxdm7&e!r0_du5glqk8r4_qrtrc1l
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['heroicatour.herokuapp.com','127.0.0.1']
 
