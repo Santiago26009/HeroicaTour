d6314e29a1d30943830f0b787f77daff7466e6ec
Santiago Zúñiga <39531781+Santiago26009@users.noreply.github.com>
2021-05-10 13:36:11 -0500

    Update Database Settings

--- HeroicaTour_API/settings_pr.py
+++ HeroicaTour_API/settings_pr.py
@@ -4,15 +4,12 @@ import dj_database_url
 from decouple import config
 
 DATABASES = {
-    #'default':{
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #    'NAME': 'd7h1j5v5oit3iu',
-    #    'USER': 'xgqxdmongnceoh',
-    #    'PASSWORD':'b8424c26f8edf906c89eca50892e45d637ae74f1baaa45b58a27da12f756245b',
-    #    'HOST': 'ec2-54-224-194-214.compute-1.amazonaws.com',
-    #    'PORT': 5432
-    #}
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd7h1j5v5oit3iu',
+        'USER': 'xgqxdmongnceoh',
+        'PASSWORD':'b8424c26f8edf906c89eca50892e45d637ae74f1baaa45b58a27da12f756245b',
+        'HOST': 'ec2-54-224-194-214.compute-1.amazonaws.com',
+        'PORT': 5432
+    }
 }
