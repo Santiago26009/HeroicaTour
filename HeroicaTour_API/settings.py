af1bb8a0f1647fa7d01258c04d3d5dfe13280acb
Santiago Zúñiga <39531781+Santiago26009@users.noreply.github.com>
2024-04-22 20:55:17 +0200

    Update settings.py

--- HeroicaTour_API/settings.py
+++ HeroicaTour_API/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #    'NAME': 'HeroicaTour',
     #    'USER': 'postgres',
-    #    'PASSWORD':'soldiersz7',
+    #    'PASSWORD':'asdasdomodcadi',
     #    'HOST': '127.0.0.1',
     #    'PORT': 5432
     #}
